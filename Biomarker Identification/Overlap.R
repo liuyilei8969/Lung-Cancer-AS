f3d84af2aabfd80cf24f867ffd2b84d8f0fe2091
liuyilei8969 <ylliu8969@gmail.com>
2025-01-14 18:48:37 +0800

    Update Overlap.R

--- Biomarker Identification/Overlap.R
+++ Biomarker Identification/Overlap.R
@@ -1,3 +1,16 @@
 library(openxlsx)
 
+# 读取DS事件
+DS <- read.xlsx("/path/to/DS/events.xlsx",sheet =2)
+DS <- c(DS$up,DS$dn)
+DS <- na.omit(DS)
+
+# 读取 Boruta 结果
 Boruata_features <- read.xlsx('/path/to/Boruta_features.xlsx')
+Boruata_features <- Boruata_features[1]
+
+# 取交集
+markers <- intersect(Boruata_features, DS)
+markers <- as.data.frame(markers)
+
+write.xlsx(markers,'markers.xlsx')
