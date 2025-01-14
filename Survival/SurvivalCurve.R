6661446bbd6b7b3680370cf9eb5bed808da55b3a
liuyilei8969 <ylliu8969@gmail.com>
2025-01-14 20:28:21 +0800

    Update SurvivalCurve.R

--- Survival/SurvivalCurve.R
+++ Survival/SurvivalCurve.R
@@ -1,1 +1,12 @@
+library("survival")
+library("survminer")
+library("openxlsx")
 
+lung = read.xlsx('survival.xlsx') # xlsx中有分组信息，OS，OS.time
+
+fit <- survfit(Surv(OS.time, OS) ~ PSI, data = lung)
+summary(fit)
+
+gg <- ggsurvplot(fit,
+           pval = TRUE, 
+           linetype = "strata")
