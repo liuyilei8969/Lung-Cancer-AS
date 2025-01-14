e1f3588204ea8291997af9fd78d0a49243c1b8f4
liuyilei8969 <ylliu8969@gmail.com>
2025-01-14 18:13:38 +0800

    Update Count.py

--- RBP Target Enrich/Count.py
+++ RBP Target Enrich/Count.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # 读取ExS矩阵
-rbp_matrix = pd.read_csv('../result/subtype/ExDS.txt', sep = '\t') 
+rbp_matrix = pd.read_csv('DExDS.txt', sep = '\t') # 或者其他的ExS矩阵
 # 创建一个空的字典来存储每个RBP的activation和repression计数
 activation_repression_count = {}
 
-# 假设RBP行为矩阵中，第一列是剪接事件名称，其他列是RBP的行为数据
+# 遍历计数
 for rbp in rbp_matrix.columns[1:]:  # 跳过第一列（剪接事件名称）
-    # 统计每个RBP的-1和1的数量
+    # 统计每个RBP列的 repression 和 activation 的数量
     repression_count = (rbp_matrix[rbp] == -1).sum()
     activation_count = (rbp_matrix[rbp] == 1).sum()
 
@@ -19,4 +19,4 @@ for rbp in rbp_matrix.columns[1:]:  # 跳过第一列（剪接事件名称）
 activation_repression_df = pd.DataFrame.from_dict(activation_repression_count, orient='index')
 
 # 输出结果
-activation_repression_df.to_csv('../result/subtype/allDScount.txt', sep = '\t')
+activation_repression_df.to_csv('DE_DS_count.txt', sep = '\t')
