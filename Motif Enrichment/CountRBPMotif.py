45bcc1b5d7da90855e2a237ccd9510ebbd19adb2
liuyilei8969 <ylliu8969@gmail.com>
2025-01-14 15:48:04 +0800

    Update CountRBPMotif.py

--- Motif Enrichment/CountRBPMotif.py
+++ Motif Enrichment/CountRBPMotif.py
@@ -4,7 +4,7 @@ import pandas as pd
 motif_rbp = pd.read_csv('motif-RBP.txt', sep='\t', header=None, names=['Motif', 'RBP'])
 
 # 读取第二个表 (Motif - Count)
-motif_count = pd.read_csv('DS_motif_results.txt', sep='\t', header=None, names=['Motif', 'Count'])
+motif_count = pd.read_csv('DS_exon_motif_results.txt', sep='\t', header=None, names=['Motif', 'Count']) #或为DS_upstream_motif_results.txt，DS_downstream_motif_results.txt
 motif_count['Count'] = pd.to_numeric(motif_count['Count'], errors='coerce')
 # 合并两个表格，找到Motif对应的Count
 merged = pd.merge(motif_rbp, motif_count, on='Motif', how='inner')
@@ -13,4 +13,4 @@ merged = pd.merge(motif_rbp, motif_count, on='Motif', how='inner')
 rbp_count = merged.groupby('RBP')['Count'].sum().reset_index()
 
 # 输出结果
-rbp_count.to_csv('DS_RBP_results.txt', sep='\t')
+rbp_count.to_csv('DS_RBP_exon_results.txt', sep='\t')   #DS_RBP_upstream_results.txt，DS_RBP_downstream_results.txt
