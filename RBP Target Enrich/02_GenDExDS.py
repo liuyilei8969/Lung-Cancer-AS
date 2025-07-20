22e1cede44781e060ebadabe562c836601f59e35
liuyilei8969 <ylliu8969@gmail.com>
2025-07-20 17:45:55 +0800

    Update 02_GenDExDS.py

--- RBP Target Enrich/02_GenDExDS.py
+++ RBP Target Enrich/02_GenDExDS.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-# 读取相关文件
+# Read relevant files
 ExS = pd.read_csv('ExS.txt', sep='\t') 
 
 # Correctly reading down-regulated files for events and RBPs
@@ -9,15 +9,15 @@ dn_events = set(pd.read_csv('dn_events.txt', header=None)[0])  # Fixed to read '
 up_rbps = set(pd.read_csv('up_RBP.txt', header=None)[0]) 
 dn_rbps = set(pd.read_csv('dn_RBP.txt', header=None)[0])  # Fixed to read 'dn_RBP.txt'
 
-# 筛选出矩阵中关注的剪接事件和RBP
+# Filter the matrix to include only the events and RBPs of interest
 filtered_events = up_events | dn_events
 filtered_rbps = up_rbps | dn_rbps
 
-# 保留矩阵中出现在filtered_events中的行，filtered_rbps中的列
+# Retain rows from the matrix that are in filtered_events and columns that are in filtered_rbps
 filtered_matrix = ExS[ExS['name'].isin(filtered_events)]
 filtered_matrix = filtered_matrix[['name'] + [rbp for rbp in filtered_matrix.columns[1:] if rbp in filtered_rbps]]
 
-# 修改矩阵值
+# Modify matrix values
 corrected_matrix = filtered_matrix.copy()
 
 # Vectorized approach to modify values in the matrix based on conditions
@@ -35,7 +35,7 @@ for event in filtered_matrix['name']:
         elif event in dn_events and rbp in up_rbps:
             corrected_matrix.loc[up_mask, rbp] = filtered_matrix.loc[up_mask, rbp].where(filtered_matrix[rbp] == -1, 0)
 
-# 保存修改后的矩阵
+# Save the modified matrices
 filtered_matrix.to_csv('DExDS.txt', index=False, sep='\t')
 corrected_matrix.to_csv('correctDExDS.txt', index=False, sep='\t')
 
