f60ae6fce9e16aa59b23d1b3326bafc537a69f8e
liuyilei8969 <ylliu8969@gmail.com>
2025-07-20 17:20:27 +0800

    Update ConstructModel.R

--- Survival/ConstructModel.R
+++ Survival/ConstructModel.R
@@ -2,22 +2,22 @@ library("survival")
 library("survminer")
 library("openxlsx")
 
-# 加载数据，MultiCovariate.R 的结果
+# Load data, results from MultiCovariate.R
 result <- read.xlsx('MultiCovariate.xlsx')
-result <- result[order(result$FDR), ]
+result <- result[order(result$FDR), ]  # Sort the results by FDR
 
-# 挑选变量
-num_selected <- 2 # 输入一个数字
-survival_selected <- survival[,rownames(result[1:num_selected,])]
-survival_selected$OS <- survival$OS
-survival_selected$OS.time <- survival$OS.time
+# Select variables
+num_selected <- 2 # Input a number to specify how many variables to select
+survival_selected <- survival[, rownames(result[1:num_selected,])]  # Select the variables based on row names in 'result'
+survival_selected$OS <- survival$OS  # Add survival status to the data
+survival_selected$OS.time <- survival$OS.time  # Add survival time to the data
 
-# 建立预后模型
-coxphdata = coxph(Surv(OS.time,OS)~.,data=survival_selected) 
-coefficients = as.matrix(coxphdata$coefficients)
-survival_selected = survival_selected[,1:num_selected]
-survival_selected = as.matrix(survival_selected)
-risk = t(coefficients) %*% t(survival_selected)
+# Build the prognostic model using Cox proportional hazards regression
+coxphdata <- coxph(Surv(OS.time, OS) ~ ., data = survival_selected)  # Fit Cox model
+coefficients <- as.matrix(coxphdata$coefficients)  # Extract coefficients
+survival_selected <- survival_selected[, 1:num_selected]  # Select the first 'num_selected' columns for the model
+survival_selected <- as.matrix(survival_selected)  # Convert to matrix
+risk <- t(coefficients) %*% t(survival_selected)  # Calculate risk score
 
-# 保存结果
-write.xlsx(risk,'risk.xlsx')
+# Save the result to an Excel file
+write.xlsx(risk, 'risk.xlsx')
