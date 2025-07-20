71bf32b54fb6cc70f10f2e5ea30f032c8bca038f
liuyilei8969 <ylliu8969@gmail.com>
2025-07-20 17:22:16 +0800

    Update SurvivalCurve.R

--- Survival/SurvivalCurve.R
+++ Survival/SurvivalCurve.R
@@ -2,11 +2,14 @@ library("survival")
 library("survminer")
 library("openxlsx")
 
-lung = read.xlsx('survival.xlsx') # xlsx中有分组信息，OS，OS.time
+# Load data from the 'survival.xlsx' file, which contains grouping information, OS, and OS.time
+lung = read.xlsx('survival.xlsx')
 
-fit <- survfit(Surv(OS.time, OS) ~ PSI, data = lung)
-summary(fit)
+# Fit survival curve based on PSI (possibly a variable indicating a certain condition)
+fit <- survfit(Surv(OS.time, OS) ~ PSI, data = lung)  # Create a Kaplan-Meier survival fit for OS based on PSI
+summary(fit)  # Print the summary of the survival fit
 
+# Plot the survival curve
 gg <- ggsurvplot(fit,
-           pval = TRUE, 
-           linetype = "strata")
+           pval = TRUE,  # Show p-value on the plot
+           linetype = "strata")  # Different line types for strata (groups)
