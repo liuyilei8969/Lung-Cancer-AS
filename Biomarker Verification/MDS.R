cfe92f19990d070d66adb85d322759bf01c25e43
liuyilei8969 <ylliu8969@gmail.com>
2025-07-20 17:10:56 +0800

    Update MDS.R

--- Biomarker Verification/MDS.R
+++ Biomarker Verification/MDS.R
@@ -3,49 +3,46 @@ library(dplyr)
 library(stringr)
 library(ggplot2)
 
-# 读取样本信息
-annotation <- read.xlsx('/path/to/sample/information.xlsx') # 准备xlsx表格，有样本名称和对应亚型的信息
-annotation <- cbind(annotation$Filename,annotation$Subtype,str_sub(annotation$Filename,-1))
-colnames(annotation) <- c('Sample_id','Subtype','Group')
+# Read sample information
+annotation <- read.xlsx('/path/to/sample/information.xlsx') # Prepare an xlsx file with sample names and corresponding subtypes
+annotation <- cbind(annotation$Filename, annotation$Subtype, str_sub(annotation$Filename, -1))
+colnames(annotation) <- c('Sample_id', 'Subtype', 'Group')
 annotation <- as.data.frame(annotation)
 rownames(annotation) <- annotation$Sample_id
 
-# 读取 AS matrix 文件
-AS <- read.table('splicing_matrix.txt', header = True, sep = '\t')
+# Read the AS matrix file
+AS <- read.table('splicing_matrix.txt', header = TRUE, sep = '\t')
 rownames(AS) <- AS$`#event_id`
-AS <- AS[,c(-1,-2)]
+AS <- AS[, c(-1, -2)]
 # AS <- AS[str_sub(colnames(AS), -1) == 'T', ] # Subtype-Specific Biomarkers
 
-# 读取 Biomarker 文件
+# Read Biomarker file
 markers <- read.xlsx('markers.xlsx')
 markers <- markers$markers
 
-# 取出 ASmarker 的 PSI
+# Extract PSI values for AS markers
 ASmarkers <- AS[markers,]
 
 ASmarkers <- t(apply(ASmarkers, 1, function(x) {
-  # 用行均值替换 NA 值
+  # Replace NA values with the row mean
   x[is.na(x)] <- mean(x, na.rm = TRUE)
   return(x)
 }))
 ASmarkers <- as.data.frame(ASmarkers)
 
-
-# MDS 降维
-dist <- as.matrix(dist(t(ASmarkers),method='euclidean'))
+# MDS dimensionality reduction
+dist <- as.matrix(dist(t(ASmarkers), method = 'euclidean'))
 mds <- cmdscale(dist)
-mds.data <- data.frame(Sample=rownames(mds),
-                       X=mds[,1],
-                       Y=mds[,2])
+mds.data <- data.frame(Sample = rownames(mds),
+                       X = mds[,1],
+                       Y = mds[,2])
 
-mds.data <- mds.data%>%
-  mutate(Group=str_sub(Sample,-1)) # Tumor-Specific Biomarkers
-mds.data$Group = ifelse(mds.data$Group == 'T','Tumor','Normal') # Tumor-Specific Biomarkers
+mds.data <- mds.data %>%
+  mutate(Group = str_sub(Sample, -1)) # Tumor-Specific Biomarkers
+mds.data$Group = ifelse(mds.data$Group == 'T', 'Tumor', 'Normal') # Tumor-Specific Biomarkers
 
 mds.data$Group <- annotation[rownames(mds.data),]$Subtype # Subtype-Specific Biomarkers
 
-# 可视化
-ggplot(data=mds.data, aes(x=X, y=Y, fill=Group, color=Group)) +
-  geom_point(size=2,stroke = 1, shape = 21)
-
-
+# Visualization
+ggplot(data = mds.data, aes(x = X, y = Y, fill = Group, color = Group)) +
+  geom_point(size = 2, stroke = 1, shape = 21)
