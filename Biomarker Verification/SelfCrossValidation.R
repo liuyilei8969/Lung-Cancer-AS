caa3d016afefdf181ef74f1d467ab7d3d797d2aa
liuyilei8969 <ylliu8969@gmail.com>
2025-07-20 17:10:12 +0800

    Update SelfCrossValidation.R

--- Biomarker Verification/SelfCrossValidation.R
+++ Biomarker Verification/SelfCrossValidation.R
@@ -4,155 +4,110 @@ library(stringr)
 library(ROCR)
 library(caret)
 
-
-# 读取样本信息
-annotation <- read.xlsx('/path/to/sample/information.xlsx') # 准备xlsx表格，有样本名称和对应亚型的信息
-annotation <- cbind(annotation$Filename,annotation$Subtype,str_sub(annotation$Filename,-1))
-colnames(annotation) <- c('Sample_id','Subtype','Group')
+# Read sample information
+annotation <- read.xlsx('/path/to/sample/information.xlsx') # Prepare an xlsx file with sample names and corresponding subtypes
+annotation <- cbind(annotation$Filename, annotation$Subtype, str_sub(annotation$Filename, -1))
+colnames(annotation) <- c('Sample_id', 'Subtype', 'Group')
 annotation <- as.data.frame(annotation)
 rownames(annotation) <- annotation$Sample_id
 
-# 读取 AS matrix 文件
-AS <- read.table('splicing_matrix.txt', header = True, sep = '\t')
+# Read the AS matrix file
+AS <- read.table('splicing_matrix.txt', header = TRUE, sep = '\t')
 rownames(AS) <- AS$`#event_id`
-AS <- AS[,c(-1,-2)]
-# AS <- AS[str_sub(colnames(AS), -1) == 'T', ] # Subtype-Specific Biomarkers
+AS <- AS[, c(-1, -2)]
 
-# 读取 Biomarker 文件
-markers <- read.xlsx('markers.xlsx')
-markers <- markers$markers
+# Calculate the NA ratio for each row
+na_ratio <- apply(AS, 1, function(x) mean(is.na(x)))
 
-# 取出 ASmarker 的 PSI
-ASmarkers <- AS[markers,]
+# Calculate the standard deviation for each row
+std_dev <- apply(AS, 1, sd, na.rm = TRUE)
 
-ASmarkers <- t(apply(ASmarkers, 1, function(x) {
-  # 用行均值替换 NA 值
-  x[is.na(x)] <- mean(x, na.rm = TRUE)
-  return(x)
-}))
-ASmarkers <- as.data.frame(ASmarkers)
+# Filter the AS matrix based on NA ratio and standard deviation
+AS_filtered = AS[na_ratio < 0.7 & std_dev > 0.15, ]
 
-# 去除意义不大的 AS 事件，增加随机取样的可信度
-row_sd <- apply(AS, 1, sd, na.rm = TRUE)
-na_ratio <- rowMeans(is.na(AS))
-AS_filtered <- AS[row_sd > 0.1 & na_ratio < 0.9, ]
+# Impute missing values with the mean for each row
 AS_filtered <- t(apply(AS_filtered, 1, function(x) {
-  # 用行均值替换 NA 值
   x[is.na(x)] <- mean(x, na.rm = TRUE)
   return(x)
 }))
 
-# 提取分组信息
-Group <- substr(colnames(AS_filtered), -1) # Tumor-Specific Biomarkers
-# Group <- annotation[colnames(AS_filtered), ]$Subtype # Subtype-Specific Biomarkers
+AS_filtered = as.data.frame(t(AS_filtered))
 
-# 转置数据，使每行成为样本，列成为特征
-AS_filtered <- t(AS_filtered)
+# Add group information
+AS_filtered$Group = str_sub(rownames(AS_filtered), -1)
+AS_filtered$Group = as.factor(AS_filtered$Group)
+AS_filtered$Group = ifelse(AS_filtered$Group == 'N', 0, 1)
+
+# Subtype-Specific 
+# AS_filtered$Group = annotation$Subtype
+# AS_filtered = AS_filtered[str_sub(rownames(AS_filtered), -1) == 'T', ]
+# AS_filtered = AS_filtered[AS_filtered$Group %in% c('A', 'S'), ]
+# AS_filtered$Group = as.factor(AS_filtered$Group)
+# AS_filtered$Group = ifelse(AS_filtered$Group == 'A', 0, 1)
 
 set.seed(123)
-
-# 利用 ASmarker 绘制 ROC ，并执行5次随机抽样，绘制 ROC
-roc_list <- list()
-auc_values <- numeric(6)
-
-# 用 ASmarker 做一个5 fold CV
-ASmarkers = as.data.frame(t(ASmarkers))
-ASmarkers$label = str_sub(rownames(ASmarkers),-1) # Tumor-Specific Biomarkers
-#ASmarkers$label = annotation[colnames(ASmarkers), ]$Subtype # Subtype-Specific Biomarkers
-ASmarkers$label = as.factor(ASmarkers$label)
-ASmarkers$label =ifelse(ASmarkers$label=='N',0,1) # Tumor-Specific Biomarkers
-#ASmarkers$label =ifelse(ASmarkers$label=='A',0,1) # Subtype-Specific Biomarkers
-
-folds <- createFolds(1:356, k=5)
-
-# 初始化变量以保存预测和实际值
+folds <- createFolds(1:356, k = 5)
+# folds <- createFolds(1:148, k = 5) # Subtype-Specific                
+i = 1
+trainlst = list()
+markerlst = list()
 all_predictions <- c()
 all_actuals <- c()
-# 在每一折交叉验证中进行训练和预测
+
+# Split data into folds
 for (fold in folds) {
-  train_data <- ASmarkers[-fold, ]
-  test_data <- ASmarkers[fold, ]
+  train_data <- AS_filtered[-fold, ]
+  test_data <- AS_filtered[fold, ]
+  trainlst[[i]] = rownames(train_data)
+  i = i + 1
+}
+========================================================================================================================================
+# Use the script from Biomarker Identification to find features for each fold from the trainlst, saving the results in markerlst
+========================================================================================================================================
+i = 1
+for (fold in folds) {
+  train_data <- AS_filtered[-fold, ]
+  test_data <- AS_filtered[fold, ]
   
-  # 训练 KNN 模型
-  pre <- knn(train = train_data[, c(1:24)], test = test_data[, c(1:24)], cl = train_data[, c(25)], k = 7, prob = TRUE)   # Tumor-Specific Biomarkers
-  #pre <- knn(train = train_data[, c(1:8)], test = test_data[, c(1:8)], cl = train_data[, c(9)], k = 7, prob = TRUE)  # Subtype-Specific Biomarkers
-  
-  # 预测概率
+  markers = markerlst[[i]]
+  # Train KNN model
+  pre <- knn(train = train_data[, c(markers)], test = test_data[, c(markers)], cl = train_data[, c('Group')], k = 7, prob = TRUE)
+  # Get prediction probabilities
   prob <- attr(pre, "prob")
   prob <- 2 * ifelse(pre == "0", 1 - prob, prob) - 1
   all_predictions <- c(all_predictions, prob)
-  all_actuals <- c(all_actuals, test_data[, c(25)])
-  #all_actuals <- c(all_actuals, test_data[, c(8)])
+  all_actuals <- c(all_actuals, test_data[, c('Group')])
+  i = i + 1
 }
+# Convert prediction probabilities to class predictions
+threshold <- 0
+predicted_classes <- ifelse(all_predictions > threshold, 1, 0)
 
-# 计算ROC和AUC
+# Convert actual labels to numeric (assuming actual labels are 0 or 1)
+actual_classes <- as.numeric(all_actuals)
+# Compute confusion matrix
+conf_matrix <- table(Predicted = predicted_classes, Actual = actual_classes)
+
+# Compute evaluation metrics
+accuracy <- sum(diag(conf_matrix)) / sum(conf_matrix)
+precision <- conf_matrix[2, 2] / sum(conf_matrix[, 2])
+recall <- conf_matrix[2, 2] / sum(conf_matrix[2, ])
+f1_score <- 2 * precision * recall / (precision + recall)
+
+# Output the results
+cat("Accuracy:", accuracy, "\n")
+cat("Precision:", precision, "\n")
+cat("Recall:", recall, "\n")
+cat("F1-Score:", f1_score, "\n")
+
+# ROC curve and AUC
 pred_knn <- prediction(all_predictions, all_actuals)
 pred_knn <- performance(pred_knn, "tpr", "fpr")
-roc_list[[1]] <- pred_knn
-result <- roc(response = all_actuals, predictor = all_predictions)
-auc_values[1] <- result$auc
+plot(pred_knn, col = 'firebrick', lwd = 1.5)
+result <- roc(response = all_actuals, predictor = all_predictions, direction = "<")
+auc_value <- auc(result)
 
-set.seed(7)
-
-for (i in 1:5) {
-  # 随机选择特征
-  selected_features <- sample(ncol(AS_filtered), 24) # Tumor-Specific Biomarkers
-  #selected_features <- sample(ncol(AS_filtered), 8) # Subtype-Specific Biomarkers
-  AS_filtered_selected <- AS_filtered[, selected_features]
-  
-  # 将分组信息添加到数据框中
-  AS_filtered_selected <- data.frame(AS_filtered_selected)
-  AS_filtered_selected$label <- Group
-  
-  # 确保目标变量是因子
-  AS_filtered_selected$label <- as.factor(AS_filtered_selected$label)
-  AS_filtered_selected$label =ifelse(AS_filtered_selected$label=='N',0,1) # Tumor-Specific Biomarkers
-  #AS_filtered_selected$label =ifelse(AS_filtered_selected$label=='A',0,1) # Subtype-Specific Biomarkers
-  
-  # 使用 K 折交叉验证
-  folds <- createFolds(1:356, k=5)
-  
-  # 初始化变量以保存预测和实际值
-  all_predictions <- c()
-  all_actuals <- c()
-  
-  # 在每一折交叉验证中进行训练和预测
-  for (fold in folds) {
-    train_data <- AS_filtered_selected[-fold, ]
-    test_data <- AS_filtered_selected[fold, ]
-    
-    # 训练 KNN 模型
-    pre <- knn(train = train_data[, 1:24], test = test_data[, 1:24], cl = train_data[, 25], k = 7, prob = TRUE) # Tumor-Specific Biomarkers
-    #pre <- knn(train = train_data[, c(1:8)], test = test_data[, c(1:8)], cl = train_data[, c(9)], k = 7, prob = TRUE)  # Subtype-Specific Biomarkers
-    
-    # 预测概率
-    prob <- attr(pre, "prob")
-    prob <- 2 * ifelse(pre == "0", 1 - prob, prob) - 1
-    all_predictions <- c(all_predictions, prob)
-    all_actuals <- c(all_actuals, test_data[, 25])
-  }
-  
-  # 计算ROC和AUC
-  pred_knn <- prediction(all_predictions, all_actuals)
-  pred_knn <- performance(pred_knn, "tpr", "fpr")
-  roc_list[[i+1]] <- pred_knn
-  result <- roc(response = all_actuals, predictor = all_predictions)
-  auc_values[i+1] <- result$auc
-}
-
-# 可视化
-par(mfrow =c(1,1),omi = c(0.2,0.2,0.2,0.2),bty = 'l',font.lab = 1, font.axis = 1,cex.axis = 1,cex.lab = 1.3)
-
-plot(roc_list[[1]], col = 'firebrick', lwd = 3, xlab = 'False Positive Rate', ylab = 'True Positive Rate', xlim = c(0, 1), ylim = c(0, 1))
-
-for (i in 2:6) {
-  plot(roc_list[[i]], add = TRUE, col = 'grey', lwd = 1.5)
-}
-
-# 添加斜对角线
-abline(a = 0, b = 1, col = 'black', lty = 2)
-
-# 添加图例
-legend('bottomright', legend = c(paste('ASmarkers', 'AUC:', round(auc_values[1], 2)),
-                                 paste('Random', 1:5, 'AUC:', round(auc_values[2:6], 2))),
-       col = c('firebrick',rep('grey', 5) ), lwd = 2)
+# Precision-Recall curve and AUC
+pr = pr.curve(scores.class0 = all_predictions, weights.class0 = all_actuals, curve = TRUE)
+prauc_value <- pr$auc.integral
+plot(pr, col = 'firebrick', lwd = 1.5)
